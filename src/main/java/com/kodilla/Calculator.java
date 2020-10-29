63eecf141da9f26f183103511f5bc15b6b284547
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2020-10-29 12:37:11 +0100

    Add 2 methods

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,22 @@
 package com.kodilla;
 
-public class Calculator {
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addition(7.3, 4.5));
+        System.out.println(calculator.subtraction(5.6, 2.3));
+    }
+
+    public double addition (double x, double y)
+    {
+        return x+y;
+    }
+
+    public double subtraction (double x, double y)
+    {
+        return x-y;
+    }
+
 }
